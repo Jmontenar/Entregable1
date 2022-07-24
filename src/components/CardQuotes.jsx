98f4dbc548498fc45f76330d9d7a937d9924e903
Jmontenar <jmontenar@gmail.com>
2022-07-24 17:47:21 -0500

    Entregable1 React

---
+++ src/components/CardQuotes.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const CardQuotes = ({randomQuotes, randomColors, getRandomAll}) => {
+   
+   const objectStyle = {
+    color: randomColors
+}
+const objectStyle2 = {
+    backgroundColor: randomColors    
+}
+
+    return (
+    
+    <article className='Card' style={objectStyle}>
+        <header>
+
+        </header>
+        <div className='Card__container'>
+        <i className='fa-solid fa-quote-left'></i>
+        <span className='Card__quote'>{randomQuotes.quote}</span><p className='Card__author'>{randomQuotes.author}</p>
+        <button onClick={getRandomAll}className='Card__button' style={objectStyle2}>&#62;</button>
+        </div>
+    
+        
+        </article>
+  )
+}
+
+export default CardQuotes
